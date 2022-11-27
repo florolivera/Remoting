f2136770a1f08581dd193ac2a36c6153c20ae7ee
Florencia <117230347+florolivera@users.noreply.github.com>
2022-11-27 20:45:10 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+
+namespace Remoting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpChannel chan = new TcpChannel(8085);
+            ChannelServices.RegisterChannel(chan);
+
+            RemotingConfiguration.RegisterWellKnownServiceType(System.Type.GetType("SeverClass.myRemoteClass, ServerClass"), "RemoteTest",
+            WellKnownObjectMode.SingleCall);
+
+            System.Console.WriteLine("Hit <enter> to exit..");
+            System.Console.ReadLine();
+        }
+            
+    }
+}
